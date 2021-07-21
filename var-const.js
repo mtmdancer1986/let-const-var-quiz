8a978e781a58afaf00aeae176b18914a72f28280
McKay <mtmdancer@hotmail.com>
2021-07-21 13:01:50 -0400

    var/const quiz

---
+++ var-const.js
@@ -0,0 +1,20 @@
+var PI = 3.14;
+PI = 42;
+
+// const PI= 3.14; Solution
+
+1)
+// var = hoisted; can redeclare/reassign ; 
+// let = block Scoped; cant redeclare, can reassign 
+
+2)
+// var = hoisted; can redeclare; 
+// const = cant redeclare or assign ; block scope;
+
+3)
+// let = block Scoped; cant redeclare, can reassign 
+// const = cant redeclare or assign ; block scope;
+
+4)
+//  variables are lifted to a broader scope of the entire js document;
+// functions variables can be declare/define before intialization;
